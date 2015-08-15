49df2c2cf4869d0cbd9abbfca570010ec18daccf
Philip Karpiak <philip@eswat.ca>
2015-08-15 13:58:33 -0500

    Handle different name values between data

--- tools/datacleanup.js
+++ tools/datacleanup.js
@@ -2,23 +2,37 @@ var path               = require('path'),
     fs                 = require('fs'),
     jsonq              = require('jsonq'),
     heroesJson         = require('../data/heroes.json'),
+    correctionsJson    = require('../data/corrections.json'),
     talentBuildsJson   = require('../data/herotalentbuilds.json'),
     winPercentagesJson = require('../data/herowinpercentages.json');
 
 var heroData = [],
-    heroTalentBuilds = talentBuildsJson.results,
-    heroWinPercentages = winPercentagesJson.results;
+    correctNames = jsonq(correctionsJson),
+    heroTalentBuilds = talentBuildsJson.results.talents,
+    heroWinPercentages = winPercentagesJson.results.heroes;
 
+// Populate heroData with base statistics
 for (var i = 0; i < heroesJson.length; i++) {
   var hero = heroesJson[i];
+
   heroData.push({
     "id": i + 1,
-    "name": hero.name,
+    "name": checkName(hero.name),
     "role": hero.role,
     "franchise": hero.franchise,
     "ratings": hero.ratings,
     "talents": hero.talents
   });
+}
+
+
+function checkName(name) {
+  foundMatch = "";
+  var foundMatch = correctNames.find('name', function () {
+    return this.toString() === name;
+  });
+
+  return foundMatch.length ? foundMatch.sibling('correctName').value()[0] : name;
 }
 
 fs.writeFile(
