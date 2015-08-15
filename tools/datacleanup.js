860ba9d4081304d67e10ec7ed37ef9d4b9bbb114
Philip Karpiak <philip@eswat.ca>
2015-08-15 13:58:42 -0500

    Add win percentages to heroData.json

--- tools/datacleanup.js
+++ tools/datacleanup.js
@@ -13,26 +13,25 @@ var heroData = [],
 
 // Populate heroData with base statistics
 for (var i = 0; i < heroesJson.length; i++) {
-  var hero = heroesJson[i];
+  var hero = heroesJson[i],
+      heroName = checkName(hero.name),
+      winPercentages = jsonq(heroWinPercentages);
+
+  var heroWinPercent = winPercentages.find('name', function () {
+    return this.text === heroName;
+  })
+    .sibling('winPercent')
+    .value()[0]
 
   heroData.push({
     "id": i + 1,
-    "name": checkName(hero.name),
+    "name": heroName,
     "role": hero.role,
     "franchise": hero.franchise,
+    "winPercent": heroWinPercent.substring(0, heroWinPercent.length - 2),
     "ratings": hero.ratings,
     "talents": hero.talents
   });
-}
-
-
-function checkName(name) {
-  foundMatch = "";
-  var foundMatch = correctNames.find('name', function () {
-    return this.toString() === name;
-  });
-
-  return foundMatch.length ? foundMatch.sibling('correctName').value()[0] : name;
 }
 
 fs.writeFile(
@@ -43,3 +42,12 @@ fs.writeFile(
     console.log('Data written');
   }
 );
+
+function checkName(name) {
+  foundMatch = "";
+  var foundMatch = correctNames.find('name', function () {
+    return this.toString() === name;
+  });
+
+  return foundMatch.length ? foundMatch.sibling('correctName').value()[0] : name;
+}
