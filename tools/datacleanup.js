0f6e5fbdf4e0881a8adace8434ae98f8e5c73d3e
Philip Karpiak <philip@eswat.ca>
2015-08-15 12:32:42 -0500

    Only get necessary data from heroes.json

--- tools/datacleanup.js
+++ tools/datacleanup.js
@@ -1,17 +1,29 @@
 var path               = require('path'),
     fs                 = require('fs'),
     jsonq              = require('jsonq'),
-    heroes             = require('../data/heroes.json'),
-    heroTalentBuilds   = require('../data/herotalentbuilds.json'),
-    heroWinPercentages = require('../data/herowinpercentages.json');
+    heroesJson         = require('../data/heroes.json'),
+    talentBuildsJson   = require('../data/herotalentbuilds.json'),
+    winPercentagesJson = require('../data/herowinpercentages.json');
 
-var heroData = {};
+var heroData = [],
+    heroTalentBuilds = talentBuildsJson.results,
+    heroWinPercentages = winPercentagesJson.results;
 
-heroData = JSON.stringify(heroes);
+for (var i = 0; i < heroesJson.length; i++) {
+  var hero = heroesJson[i];
+  heroData.push({
+    "id": i + 1,
+    "name": hero.name,
+    "role": hero.role,
+    "franchise": hero.franchise,
+    "ratings": hero.ratings,
+    "talents": hero.talents
+  });
+}
 
 fs.writeFile(
-  path.join(__dirname, '../src/data/heroData.json')
-  heroData,
+  path.join(__dirname, '../src/data/heroData.json'),
+  JSON.stringify(heroData),
   function (err) {
     if (err) throw err;
     console.log('Data written');
